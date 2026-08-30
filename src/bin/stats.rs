0e713e26ad81067fd7f5963fbabdf2d8bf14d4f8
Trevor Gamblin <tgamblin@ecocode.ca>
2026-08-29 21:56:03 -0400

    src: bin: stats.rs: fix arg typing, update rolling
    Signed-off-by: Trevor Gamblin <tgamblin@ecocode.ca>

--- src/bin/stats.rs
+++ src/bin/stats.rs
@@ -1,3 +1,4 @@
+use dice_parser::{DiceExpr, RollSpec, Keep};
 use glog_v2_character_generator::{CharacterGenerator};
 use clap::Parser;
 
@@ -6,26 +7,26 @@ use clap::Parser;
 struct StatArgs {
     // number of stats to roll
     #[arg(short, long, default_value_t = 6)]
-    stats: u8,
+    stats: u32,
 
     // number of dice to roll per stat
     #[arg(short, long, default_value_t = 3)]
-    dice: u8,
+    dice: u32,
 
     // number of faces per die
     #[arg(short, long, default_value_t = 6)]
-    faces: u8,
+    faces: u32,
 
     // how many low rolls should be ignored
     #[arg(short, long, default_value_t = 0)]
-    lowest: u8,
+    lowest: u32,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args = StatArgs::parse();
-    let mut rng = rand::thread_rng();
-    let result = CharacterGenerator::roll_ability_score(&mut rng, args.dice, args.faces, args.lowest);
+    let mut expr = DiceExpr::Roll(RollSpec::new(args.dice, args.faces, Some(Keep::Highest(args.dice-args.lowest))));
+    let result = CharacterGenerator::generate_score(&mut expr);
 
     println!("Roll {}d{} drop {} lowest: {}", args.dice, args.faces, args.lowest, result);
 
